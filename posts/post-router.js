b907eb6746f121f8d620a06680cf84b79db6cb2c
tauan-longaretti <tauanlongaretti@gmail.com>
2020-04-10 15:18:06 -0400

    put and delete implemented

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,32 +1,69 @@
-const express = require('express');
+const express = require("express");
 
 //database access using knex
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 
 const router = express.Router();
 
 // /api/posts
-router.get('/', (req, res) => {
-    db('accounts')
-        .then(accounts => {
-            res.json(accounts);
-        })
-        .catch(err => {
-            res.status(500).json({ message: "error retrieving accounts", err});
-        });
+router.get("/", (req, res) => {
+  db("accounts")
+    .then((accounts) => {
+      res.json(accounts);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "error retrieving accounts", err });
+    });
 });
 
-router.post('/', (req, res) => {
-    const accountData = req.body
+router.post("/", (req, res) => {
+  const accountData = req.body;
 
-    db('accounts')
+  db("accounts")
     .insert(accountData)
-    .then(account => {
-        res.status(201).json(account);
+    .then((account) => {
+      res.status(201).json(account);
     })
-    .catch(err => {
-        res.status(500).json({ message: "failed to create new accounts", err })
+    .catch((err) => {
+      res.status(500).json({ message: "failed to create new accounts", err });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db("accounts")
+    .where({ id })
+    .update(changes)
+    .then((count) => {
+      if (count) {
+        res.json({ update: count });
+      } else {
+        res.status(404).json({ message: "invalid id" });
+      }
     })
-})
+    .catch((error) => {
+      res.status(500).json({ message: "error updating account", error });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+    const {id} = req.params;
+
+    db("accounts")
+    .where({ id })
+    .del({ id })
+    .then((count) => {
+        if (count) {
+            res.json({ deleted: count });
+        } else {
+            res.status(404).json({ message: "invalid id" });
+        }
+    })
+    .catch((error) => {
+        res.status(500).json({ message: "error deleting account", error });
+    });
+});
 
 module.exports = router;
