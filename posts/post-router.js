05c5766ae379a0d27b7bbba7585cba58285db725
tauan-longaretti <tauanlongaretti@gmail.com>
2020-04-10 15:22:39 -0400

    get by id implemented, MVP completed

--- posts/post-router.js
+++ posts/post-router.js
@@ -16,6 +16,19 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+    const {id} = req.params;
+
+    db("accounts")
+    .where({ id })
+    .then((account) => {
+        res.json(account);
+    })
+    .catch((err) => {
+        res.status(500).json({ message: "there was an error retrieving the account", err })
+    })
+})
+
 router.post("/", (req, res) => {
   const accountData = req.body;
 
